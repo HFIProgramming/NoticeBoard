d5de4f5286fd06244d676951206f0aee705b7cda
yhylord <yhylord@gmail.com>
2016-06-20 22:10:05 +0800

    Add user for migration and model

--- app/User.php
+++ app/User.php
@@ -12,7 +12,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'class', 'avatar', 'powerschool_id', 'type'
     ];
 
     /**
@@ -23,4 +23,14 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * Set the user's powerschool id.
+     *
+     * @param string $id
+     */
+    public function setPowerschoolIdAttribute($id)
+    {
+        $this->attributes['powerschool_id'] = empty($id) ? NULL : $id;
+    }
 }
--- database/migrations/2016_06_20_215205_create_users_table.php
+++ database/migrations/2016_06_20_215205_create_users_table.php
@@ -17,6 +17,10 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('class')->nullable();
+            $table->string('avatar')->nullable();
+            $table->string('powerschool_id')->nullable()->unique();
+            $table->string('type');
             $table->rememberToken();
             $table->timestamps();
         });
