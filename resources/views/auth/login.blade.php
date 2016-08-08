a7937c81f2d3b9577858c36b52ec7c6f58663d0e
yhylord <yhylord@gmail.com>
2016-08-08 23:00:19 +0800

    Reformat login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,28 +9,28 @@
 @endsection
 
 @section('content')
-    {{--TODO: Add frontend validation via Foundation Abide--}}
-<div class="row">
-    <div class="small-12 medium-6 medium-centered columns">
-        <form method="post" action="{{ url('/login') }}">
-            <div id="login-form" class="row column">
-                @if (count($errors))
-                    <div class="alert callout">
-                        <p><i class="fi-alert"></i> Incorrect email or password.</p>
-                    </div>
-                @endif
-                {{ csrf_field() }}
-                <h4 class="text-center">Log in with your email account</h4>
-                <input type="email" placeholder="Email" name="email" value="{{ old('email') }}">
-                <input type="password" placeholder="Password" name="password">
-                <input id="remember-me" type="checkbox" name="remember">
-                <label for="remember-me" class="checkbox">Remember me</label>
-                <p>
-                    <button type="submit" class="button ink expanded">Log In</button>
-                </p>
-                <p class="text-center"><a href="{{ url('/password/reset') }}">Forgot your password?</a></p>
-            </div>
-        </form>
+    {{--TODO Add frontend validation via Foundation Abide--}}
+    <div class="row">
+        <div class="small-12 medium-6 medium-centered columns">
+            <form method="post" action="{{ url('/login') }}">
+                <div id="login-form" class="row column">
+                    @if (count($errors))
+                        <div class="alert callout">
+                            <p><i class="fi-alert"></i> Incorrect email or password.</p>
+                        </div>
+                    @endif
+                    {{ csrf_field() }}
+                    <h4 class="text-center">Log in with your email account</h4>
+                    <input type="email" placeholder="Email" name="email" value="{{ old('email') }}">
+                    <input type="password" placeholder="Password" name="password">
+                    <input id="remember-me" type="checkbox" name="remember">
+                    <label for="remember-me" class="checkbox">Remember me</label>
+                    <p>
+                        <button type="submit" class="button ink expanded">Log In</button>
+                    </p>
+                    <p class="text-center"><a href="{{ url('/password/reset') }}">Forgot your password?</a></p>
+                </div>
+            </form>
+        </div>
     </div>
-</div>
 @endsection
