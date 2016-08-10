f77829fb83cd0a707e9e080d898c2077268b7bbc
yhylord <yhylord@gmail.com>
2016-08-10 23:01:31 +0800

    Move login.css to resources/assets/css

--- gulpfile.js
+++ gulpfile.js
@@ -16,21 +16,9 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    /*mix.sass('app.scss', paths.output + 'css', {
-        includePaths: [
-            paths.foundation + 'scss'
-        ]
-    });*/
     mix.copy(paths.foundation + 'css/material-foundation.css', paths.output + 'css');
+    mix.styles('login.css');
 
-    /*mix.babel([
-        paths.foundation + 'bower_components/what-input/what-input.js',
-        paths.foundation + 'bower_components/jquery/dist/jquery.js',
-        paths.foundation + 'bower_components/foundation-sites/dist/foundation.js',
-        paths.foundation + 'js/src/ripple.js',
-        paths.foundation + 'js/src/switches.js',
-        paths.foundation + 'js/src/material-foundation.js'
-    ], paths.output + 'js/foundation.js', './');*/
     mix.copy(paths.foundation + 'js/material-foundation.js', paths.output + 'js/foundation.js');
     
     mix.version(['assets/css/app.css', 'assets/css/login.css', 'assets/js/foundation.js']);
