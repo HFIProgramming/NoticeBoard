74c6af1bbd1688fff144df21057a49186c4031ca
NeverBehave <luoxinhao77@gmail.com>
2016-08-27 23:18:24 +0800

    Revert "created tables for apps"
    This reverts commit 833085091987f75b57ac4b3ee1a0ca91e40937d9.

--- database/migrations/2016_06_20_215205_create_users_table.php
+++ database/migrations/2016_06_20_215205_create_users_table.php
@@ -18,20 +18,9 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->string('class')->nullable();
-            $table->string('grade');
             $table->string('avatar')->nullable();
             $table->string('powerschool_id')->nullable()->unique();
             $table->string('type');
-            $table->string('created_counts'); //累计发言数
-            $table->string('identity'); // 用|分割不同身份,比如 1|2|3
-            //个人信息
-            $table->string('nickname');
-            $table->string('condition');
-            $table->text('self_intro');
-            $table->string('wechat_ID');
-            $table->string('QQ');
-            $table->string('alternative_email');
-            $table->string('phone');
             $table->rememberToken();
             $table->timestamps();
         });
